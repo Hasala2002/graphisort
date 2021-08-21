3f781a96c881cd988f99460ef7bc8c85236b3473
Hasala2002 <hasala.h@hotmail.com>
2021-08-21 11:07:01 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Graphisort
+Graphisort is a cool and interesting interface to visualize sorting algorithms.
+
+## Web Deployment
+You can check out the Netlify Deployment here: https://c19st.netlify.app
+
+## Issues
+
+- Not responsive
+- Only two sorting algorithms work right now
+- Add a informational section to give an explanation of the currently selected sorting algorithm
---
+++ assets/js/app.js
@@ -0,0 +1,154 @@
+var SORTABLE_COUNT = 50
+var SORTABLE_SPEED = 50
+var SORTABLE_TYPE = 'bubble'
+$('#barCountRange').val(SORTABLE_COUNT)
+$('#speedRange').val(SORTABLE_SPEED)
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+const setVisualState = (arr) => {
+    for (var i = 1; i <= SORTABLE_COUNT; i++) {
+        $(`#sortBar${i}`).css({
+            height: `${arr[i-1]}%`,
+        })
+     }
+}
+
+const resetVisualShuffle = (arr) => {
+    arr = shuffleArray(arr);
+    for (var i = 1; i <= SORTABLE_COUNT; i++) {
+        $(`#sortBar${i}`).css({
+            height: `${arr[i-1]}%`,
+        })
+     }
+}
+
+for (var i = 1; i <= SORTABLE_COUNT; i++) {
+    $('.stage').append($(`<div class="sortable-bar" id="sortBar${i}"></div>`));
+}
+
+let sortableArray = []
+
+for (var i = 1; i <= SORTABLE_COUNT; i++) {
+    sortableArray.push(parseInt((i/SORTABLE_COUNT*100).toFixed()))
+ }
+
+sortableArray = shuffleArray(sortableArray)
+
+for (var i = 1; i <= SORTABLE_COUNT; i++) {
+    $(`#sortBar${i}`).css({
+        height: `${sortableArray[i-1]}%`,
+    })
+ }
+
+$('#shuffleButton').click(()=>{
+    resetVisualShuffle(sortableArray)
+    })
+
+$('#resetButton').click(async ()=>{
+    $('#barCountRange').css({
+        pointerEvents:'none',
+        cursor: 'wait'
+    })
+    $('#speedRange').css({
+        pointerEvents:'none',
+        cursor: 'wait'
+    })
+    // await selectionSort(sortableArray)
+    await sortingData[SORTABLE_TYPE].sort(sortableArray)
+    $('#barCountRange').css({
+        pointerEvents:'unset',
+        cursor: 'pointer'
+    })
+    $('#speedRange').css({
+        pointerEvents:'unset',
+        cursor: 'pointer'
+    })
+})
+
+$('#barCountRange').change(()=>{
+   SORTABLE_COUNT = ($('#barCountRange').val())
+   $('#count').text(`BAR COUNT : ${SORTABLE_COUNT}`)
+   $('.stage').html('')
+   for (var i = 1; i <= SORTABLE_COUNT; i++) {
+    $('.stage').append($(`<div class="sortable-bar" id="sortBar${i}"></div>`));
+    }
+    sortableArray = []
+    setVisualState(sortableArray)
+    for (var i = 1; i <= SORTABLE_COUNT; i++) {
+        sortableArray.push(parseInt((i/SORTABLE_COUNT*100).toFixed()))
+     }
+     sortableArray = sortableArray.sort((a, b) => a - b)
+     setVisualState(sortableArray)
+})
+
+
+
+$('#speedRange').change(()=>{
+    SORTABLE_SPEED = ($('#speedRange').val())
+    $('#speed').text(`SPEED : ${SORTABLE_SPEED}ms`)
+ })
+
+
+ $("[id=sortBox]").click(function () {
+    let sorter = $(this).attr("data-sort");
+    $('#sorters').find('*').removeClass('active')
+    SORTABLE_TYPE = sorter
+    $(`#sortBox[data-sort=${SORTABLE_TYPE}]`).addClass('active')
+  });
+
+const sortingData = {
+    bubble:{
+        sort: async function (arr){
+            var len = arr.length;
+            for (var i = len-1; i>=0; i--){
+              for(var j = 1; j<=i; j++){
+                if(arr[j-1]>arr[j]){
+                    var temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                    setVisualState(arr)
+                    await sleep(SORTABLE_SPEED)
+                 }
+              }
+            }
+            setVisualState(arr)
+            return arr;
+         }
+    },
+    selection:{
+        sort: async function (arr){
+            var minIdx, temp, 
+                len = arr.length;
+            for(var i = 0; i < len; i++){
+              minIdx = i;
+              for(var  j = i+1; j<len; j++){
+                 if(arr[j]<arr[minIdx]){
+                    minIdx = j;
+                    await sleep(SORTABLE_SPEED)
+                    setVisualState(arr)
+                 }
+              }
+              temp = arr[i];
+              arr[i] = arr[minIdx];
+              arr[minIdx] = temp;
+            }
+            setVisualState(arr)
+            return arr;
+          }
+    }
+}
+
+
